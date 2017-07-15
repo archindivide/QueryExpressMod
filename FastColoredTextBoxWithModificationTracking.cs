1991f2038e1c9214a58ffaf5e2e033523b9a4674
archindivide <archindivide@gmail.com>
2017-07-15 09:55:32 -0700

    Initial commit

---
+++ FastColoredTextBoxWithModificationTracking.cs
@@ -0,0 +1,26 @@
+﻿using FastColoredTextBoxNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryExpress
+{
+    public class FastColoredTextBoxWithModificationTracking : FastColoredTextBox
+    {
+        public bool Modified { get; set; }
+
+        public FastColoredTextBoxWithModificationTracking(): base()
+        {
+            TextChangedDelayed += FastColoredTextBoxWithModificationTracking_TextChangedDelayed;
+        }
+
+        private void FastColoredTextBoxWithModificationTracking_TextChangedDelayed(object sender, TextChangedEventArgs e)
+        {
+            Modified = true;
+            SyntaxHighlighter.InitStyleSchema(Language.SQL);
+            SyntaxHighlighter.SQLSyntaxHighlight(VisibleRange);
+        }
+    }
+}
